854149a6d1672817fb7635e3722356f426f97e4a
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-11-15 10:24:28 +0100

    add calculatrice challenge

--- calculatriceChallenge/index.js
+++ calculatriceChallenge/index.js
@@ -1,46 +1,45 @@
 class Calculator {
-    constructor(numbers, operation){
-        this.numbers = numbers; // numbers un tableau qui stocke les nombres
-        this.operation = operation;
+    constructor(...numbers){
+        this.numbers = numbers; // this.numbers un tableau qui stocke les nombres
     }
 
     reset(){
         //method pour reset la calculatrice
+        this.this.numbers = [];
     }
 
     addition(numbers){
-        let sum = 0;
-        for (let i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
+        let sum = this.numbers[0];
+        for (let i = 1; i < this.numbers.length; i++) {
+            sum += this.numbers[i];
         }
         return sum
     }
+
+    soustraction(numbers){
+        let result = this.numbers[0];
+        for (let i = 1; i < this.numbers.length; i++) {
+            result -= this.numbers[i];
+        }
+        return result
+    }
+
+    mutliplication(numbers){
+        let product = this.numbers[0];
+        for (let i = 1; i < this.numbers.length; i++) {
+            product *= this.numbers[i];
+        }
+        return product
+    }
+
+    division(numbers){
+        let result = this.numbers[0];
+        for (let i = 1; i < this.numbers.length; i++) {
+            if(this.numbers[i] !== 0) result /= this.numbers[i];
+        }
+        return result
+    }
 }
 
-
-class Hero extends Fighter {
-    constructor(name, strength, dexterity, image, x, y, range) {
-        super(name, strength, dexterity, image, x, y, range)
-        this.weapon = null;
-        this.shield = null;
-    }
-  
-  
-    // Calculate the total attack capacity of the fighter
-    // Calcule la capacité d'attaque totale du combattant
-    getDamage() {
-      return this.strength + (this.weapon ? this.weapon.damage : 0)
-    }
-  
-  
-    // Calculate the total defense capacity of the fighter
-    // Calcule la capacité de défense totale du combattant
-    getDefense() {
-      return this.dexterity + (this.shield ? this.shield.protection : 0)
-    }
-  
-    getRange() {
-      return this.range + (this.weapon ? this.weapon.range : 0)
-    }
-  }
-  
+const myCalculator = new Calculator(5,0)
+console.log(myCalculator.division())
